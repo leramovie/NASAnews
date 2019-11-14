708a72e461de0dee3ed203b5f2de92c5b73f7e55
leramovie <leramovie@gmail.com>
2019-11-14 17:14:33 +0300

    Initial Commit

---
+++ SpaceNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpaceNews
+//
+//  Created by Igor Mikhalev on 14.11.2019.
+//  Copyright © 2019 Parakluence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
